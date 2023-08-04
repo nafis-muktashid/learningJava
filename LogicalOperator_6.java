ac7517ea0eed87b072c9886365e02ab7a42360d6
Nafis Muktashid Arpon <arponbolche@gmail.com>
2023-08-04 20:25:44 +0600

    Add files via upload

---
+++ LogicalOperator_6.java
@@ -0,0 +1,30 @@
+package learnJava_0;
+
+import java.util.*;
+
+public class LogicalOperator_6 {
+
+	public static void main(String[] args) {
+		Scanner inpt = new Scanner(System.in);
+		
+		char ch;
+		System.out.print("Enter any chracter : ");
+		ch = inpt.next().charAt(0);
+		
+		if(ch =='A' || ch == 'a') {
+			System.out.println("Vowel");
+		}else if(ch =='E' || ch == 'e') {
+			System.out.println("Vowel");
+		}else if(ch =='I' || ch == 'i') {
+			System.out.println("Vowel");
+		}else if(ch =='O' || ch == 'o') {
+			System.out.println("Vowel");
+		}else if(ch =='U' || ch == 'u') {
+			System.out.println("Vowel");
+		}else {
+			System.out.println("Consonant");
+		}
+
+	}
+
+}
