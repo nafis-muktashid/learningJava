2eeac9e24f5e09799abb943feb00b92d8ade65c8
Nafis Muktashid Arpon <arponbolche@gmail.com>
2023-08-04 19:34:11 +0600

    Add files via upload

---
+++ IfElse_5_1.java
@@ -0,0 +1,40 @@
+package learnJava_0;
+
+import java.util.*;
+
+public class IfElse_5_1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n;
+		System.out.print("Enter any number 0-9 : ");
+		n = input.nextInt();
+		
+		if(n==0) {
+			System.out.println("Zero");
+		}else if(n==1) {
+			System.out.println("One");
+		}else if(n==2) {
+			System.out.println("Two");
+		}else if(n==3) {
+			System.out.println("Three");
+		}else if(n==4) {
+			System.out.println("Four");
+		}else if(n==5) {
+			System.out.println("Five");
+		}else if(n==6) {
+			System.out.println("Six");
+		}else if(n==7) {
+			System.out.println("Seven");
+		}else if(n==8) {
+			System.out.println("Eight");
+		}else if(n==9) {
+			System.out.println("Nine");
+		}else {
+			System.out.println("Invalid Input");
+		}
+
+	}
+
+}
