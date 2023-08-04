6a9b9a23bf4ec6c11ddb619ad5cad24e12c9f24a
Nafis Muktashid Arpon <arponbolche@gmail.com>
2023-08-04 15:19:06 +0600

    Add files via upload

---
+++ PrintAnything_1_0.java
@@ -0,0 +1,11 @@
+package learnJava_0;
+
+public class PrintAnything_1_0 {
+	public static void main(String[] args) {
+		System.out.print(" id: 101,");
+		System.out.println("title: iphone15,");
+		System.out.println("price: 1895 euros,");
+		System.out.println("description: perfect product with best image quality,");
+		System.out.println("category: phone,");	
+	}
+}
---
+++ PrintAnything_1_1.java
@@ -0,0 +1,11 @@
+package learnJava_0;
+
+public class PrintAnything_1_1 {
+	public static void main(String[] args) {
+		int id=101;
+		String title="iphone15", price="1895 euros",
+				description="perfect product with best image quality",
+				category="phone";
+		System.out.print(id+",\t"+title+",\t"+price+",\t"+description+",\t"+category+", \n");
+	}
+}
